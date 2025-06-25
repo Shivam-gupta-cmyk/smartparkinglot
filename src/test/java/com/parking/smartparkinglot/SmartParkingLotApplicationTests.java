43911d07dd6ed1a5e328797ad584dca78a6a8f62
Shivam Gupta <shvmdev@gmail.com>
2025-06-26 00:16:41 +0530

    initial commit

---
+++ src/test/java/com/parking/smartparkinglot/SmartParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.smartparkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
